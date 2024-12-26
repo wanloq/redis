70079b9a42465e0a9c4074571c301e18339e5ad6
wanloq <mcjinkz@gmail.com>
2024-12-26 14:44:08 +0100

    New update

--- app/server.go
+++ app/server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
 	"os"
 )
@@ -16,7 +15,7 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	// l=listener
+
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
@@ -24,38 +23,32 @@ func main() {
 	}
 	defer l.Close()
 
-	fmt.Println("Listening on :6379...")
-
 	for {
-
+		fmt.Println("Listening on :6379...")
 		conn, err := l.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go handleConnection(conn)
+		handleClient(conn)
 	}
-
-	//
-
 }
-func handleConnection(conn net.Conn) {
+
+func handleClient(conn net.Conn) {
+	// Ensure we close the connection after we're done
 	defer conn.Close()
-	fmt.Println("New connection from", conn.RemoteAddr())
 
-	// Echo the received data back to the client
-	if _, err := io.Copy(conn, conn); err != nil {
-		fmt.Println("Error echoing data:", err)
+	// Read data
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		return
 	}
 
+	fmt.Println("Received data", buf[:n])
+
+	// Write the same data back
+	message := []byte("+PONG\r\n")
+	n, err = conn.Write(message)
+	fmt.Printf("sent %d bytes", n)
 }
-
-// func commands(input string) string {
-// 	response := ""
-
-// 	switch input == "ping" {
-// 	case condition:
-
-// 	}
-// 	return response
-// }
