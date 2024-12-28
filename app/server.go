843be95779df98f6700cea89a1f1dd4c6ff3edd6
wanloq <mcjinkz@gmail.com>
2024-12-28 21:06:20 +0100

    New update

--- app/server.go
+++ app/server.go
@@ -4,29 +4,31 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
+var p = fmt.Println
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+	p("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		p("Failed to bind to port 6379")
 		os.Exit(1)
 	}
 	defer l.Close()
-	fmt.Println("Listening on :6379...")
+	p("Listening on :6379...")
 
 	for {
 		conn, err := l.Accept()
-		fmt.Println("new connection created")
+		p("new connection created")
 		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
+			p("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
 		go handleClient(conn)
@@ -37,18 +39,35 @@ func handleClient(conn net.Conn) {
 	// Ensure we close the connection after we're done
 	defer conn.Close()
 
-	for {
-		// Read data
-		buf := make([]byte, 1024)
-		_, err := conn.Read(buf)
-		if err != nil {
-			break
-		}
-
-		// Write new data
-		message := []byte("+PONG\r\n")
-		_, err = conn.Write(message)
-		// fmt.Printf("sent %d bytes", n)
+	// for {
+	// Read data
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	msg := string(buf[:n])
+	// p("Received: %q\n", msg)
+	if err != nil {
+		return
 	}
-	fmt.Println("done handling")
+
+	// Write new data
+	commands := (strings.Split(msg, "\r\n"))
+	// comm := len(strings.Split(msg, "\r\n"))
+	if strings.ToUpper(commands[2]) == "PING" {
+		p(msg, "PING COMMAND")
+		conn.Write([]byte("+PONG\r\n"))
+	} else if strings.ToUpper(commands[2]) == "ECHO" {
+		p(msg, "ECHO COMMAND")
+		// mLen, _ := strconv.Atoi((strings.Trim(commands[0], "*")))
+		// mLen = mLen - 1
+		// mLenStr := "*" + strconv.Itoa(mLen) + "\r\n"
+		newMsg := fmt.Sprintf("*%d\r\n%s\r\n%s\r\n%s\r\n%s\r\n", len(commands[3]), commands[3], commands[4], commands[5], commands[6])
+		// newMsg := mLenStr + strings.Join(commands[3:], "\r\n")
+		p(newMsg)
+
+		conn.Write([]byte(newMsg))
+	} else {
+		p("UNRECOGNIZED COMMAND")
+		conn.Write([]byte("-ERR unrecognized command\r\n"))
+	}
+	// }
 }
