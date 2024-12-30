22c6f73f5687ed23622ee28ab43df6dfc49cd807
wanloq <mcjinkz@gmail.com>
2024-12-30 22:00:26 +0100

    New update

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -40,34 +41,60 @@ func handleClient(conn net.Conn) {
 	defer conn.Close()
 
 	// for {
-	// Read data
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	msg := string(buf[:n])
-	// p("Received: %q\n", msg)
-	if err != nil {
-		return
-	}
+	// 	// Read data
+	// 	buf := make([]byte, 1024)
+	// 	_, err := conn.Read(buf)
+	// 	if err != nil {
+	// 		return
+	// 	}
 
-	// Write new data
-	commands := (strings.Split(msg, "\r\n"))
-	// comm := len(strings.Split(msg, "\r\n"))
-	if strings.ToUpper(commands[2]) == "PING" {
-		p(msg, "PING COMMAND")
-		conn.Write([]byte("+PONG\r\n"))
-	} else if strings.ToUpper(commands[2]) == "ECHO" {
-		p(msg, "ECHO COMMAND")
-		// mLen, _ := strconv.Atoi((strings.Trim(commands[0], "*")))
-		// mLen = mLen - 1
-		// mLenStr := "*" + strconv.Itoa(mLen) + "\r\n"
-		newMsg := fmt.Sprintf("*%d\r\n%s\r\n%s\r\n%s\r\n%s\r\n", len(commands[3]), commands[3], commands[4], commands[5], commands[6])
-		// newMsg := mLenStr + strings.Join(commands[3:], "\r\n")
-		p(newMsg)
-
-		conn.Write([]byte(newMsg))
-	} else {
-		p("UNRECOGNIZED COMMAND")
-		conn.Write([]byte("-ERR unrecognized command\r\n"))
-	}
+	// 	// Write new data
+	// 	message := []byte("+PONG\r\n")
+	// 	conn.Write(message)
 	// }
+
+	for {
+		// Read data
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf)
+		msg := string(buf[:n])
+		if err != nil {
+			continue
+		}
+
+		// Write new data
+		commands := (strings.Split(msg, "\r\n"))
+		if strings.ToUpper(commands[2]) == "PING" {
+			p(msg, "PING COMMAND")
+			conn.Write([]byte("+PONG\r\n"))
+		} else if strings.ToUpper(commands[2]) == "ECHO" {
+			p(msg, "ECHO COMMAND")
+			for i := 4; i < len(commands)-3; i += 2 {
+
+			}
+			mLen, _ := strconv.Atoi((strings.Trim(commands[0], "*")))
+			mLenStr := fmt.Sprintf("$%d\r\n", mLen)
+			newMsg := mLenStr + strings.Join(commands[3:], "\r\n")
+			conn.Write([]byte(newMsg))
+			// } else if strings.ToUpper(commands[2]) == "ECHO" {
+			// 	p(msg, "ECHO COMMAND")
+			// 	mLen, _ := strconv.Atoi((strings.Trim(commands[0], "*")))
+			// 	mLen = mLen - 1
+			// 	newMsg := ""
+			// 	if mLen > 1 {
+			// 		mLenStr := fmt.Sprintf("*%d\r\n", mLen)
+			// 		newMsg = mLenStr + strings.Join(commands[3:], "\r\n")
+			// 	} else {
+			// 		newMsg = strings.Join(commands[3:], "\r\n")
+			// 	}
+			// 	conn.Write([]byte(newMsg))
+		} else {
+			p("UNRECOGNIZED COMMAND")
+			conn.Write([]byte("-ERR unrecognized command\r\n"))
+		}
+	}
+}
+
+func bulkStrParse() {
+
 }
