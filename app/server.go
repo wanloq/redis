47e14611753dd074270a4bbdfe9299c24b8187d5
wanloq <mcjinkz@gmail.com>
2025-01-03 15:04:24 +0100

    New update

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -56,29 +57,40 @@ func handleClient(conn net.Conn) {
 }
 
 func myParser(msg string) string {
+	newMsg := ""
+	cr := "\r\n"
+	msgStr := strings.Split(msg, cr)
+	comLen, _ := strconv.Atoi(strings.Trim(msgStr[0], "*"))
+	first := msgStr[0]
+	com := strings.ToUpper(strings.Join(msgStr[2:3], "\r\n"))
+	// command := strings.ToUpper(strings.Join(msgStr[1:3], "\r\n"))
+	arg := strings.ToUpper(strings.Join(msgStr[3:], "\r\n"))
 
-	if strings.ToUpper(commands[2]) == "PING" {
-		p(msg, "PING COMMAND")
-		conn.Write([]byte("+PONG\r\n"))
-	} else if strings.ToUpper(commands[1]) == "$" {
-		p(msg, "BULK STRING RECEIVED")
-		var mStr []string
-		for i := 4; i < len(commands)-1; i += 2 {
-			mStr = append(mStr, commands[i])
+	switch {
+	case comLen == 1:
+		if com == "PING" {
+			p("SIMPLE STR")
+			newMsg = fmt.Sprintf("+PONG%s", cr)
+		} else {
+			newMsg = fmt.Sprintf("-ERR unrecognized command%s", cr)
 		}
-		mLen := strings.Join(mStr, " ")
-		// mLen, _ := strconv.Atoi((strings.Trim(commands[0], "*")))
-		mComp := fmt.Sprintf("$%d\r\n", len(mLen))
-		newMsg := fmt.Sprintf("%s\r\n%s\r\n", mComp, mLen)
-		// newMsg := mComp + strings.Join(commands[3:], "\r\n")
-		p(mLen)
-		p(mComp)
-		p(newMsg)
-		conn.Write([]byte(newMsg))
-	} else {
-		p("UNRECOGNIZED COMMAND")
-		conn.Write([]byte("-ERR unrecognized command\r\n"))
+	case comLen >= 2:
+		if com == "ECHO" {
+			p("BULK STR")
+			newMsg = fmt.Sprintf("%s%s%s%s", first, cr, arg, cr)
+			// newMsg = fmt.Sprintf("%s%s%s%s", first, cr, command, cr)
+		} else {
+			newMsg = fmt.Sprintf("-ERR unrecognized command%s", cr)
+		}
+		// default:
+		// 	p("UNRECOGNIZED COMMAND")
 	}
+	// p("Arg:", arg)
+	// p("MsgStr:", msgStr)
+	// p("ComLen:", comLen)
+	// p("Command:", command)
+	// p("NewMsg:", newMsg)
+	return newMsg
 }
 
 // func myParser(msg string) string{
